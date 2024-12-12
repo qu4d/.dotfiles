862526ba2b85936965beff5e24b5b25dbcdf79e4
Kirill Velichkin <k.velichkin@gmail.com>
2024-12-12 11:56:24 +0300

    Update aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,8 +22,8 @@ fzf_configure_bindings --directory=\cF --processes=\cP --git_log= --git_status=
 alias n='nvim'
 
 # brew
-alias brewery='brew update && brew upgrade && brew autoremove && brew cleanup && brew doctor'
-alias brewery-greedy='brew update && brew upgrade --greedy && brew autoremove && brew cleanup && brew doctor'
+alias br='brew update && brew upgrade && brew autoremove && brew cleanup && brew doctor'
+alias brg='brew update && brew upgrade --greedy && brew autoremove && brew cleanup && brew doctor'
 
 # phockup
 alias phockup-preview='phockup ~/Downloads/photos "/Volumes/Seagate 1TB/photos" -d "YYYY/MM" -y'
