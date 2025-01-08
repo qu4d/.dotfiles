eaa838230c9d5cbe8cbe9aaa10ee569f990850d7
Kirill Velichkin <k.velichkin@gmail.com>
2025-01-08 19:52:29 +0300

    Add wezterm shortcuts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,8 @@
 local wezterm = require("wezterm")
-local mux = wezterm.mux
 local config = {}
 
 wezterm.on("gui-startup", function()
-	local _, _, window = mux.spawn_window({})
+	local _, _, window = wezterm.mux.spawn_window({})
 	window:gui_window():set_position(0, 0)
 	window:gui_window():toggle_fullscreen()
 end)
@@ -16,6 +15,10 @@ config = {
 	default_cursor_style = "BlinkingBlock",
 	font = wezterm.font("FiraCode Nerd Font"),
 	hide_tab_bar_if_only_one_tab = true,
+	keys = {
+		{ key = "[", mods = "CTRL|ALT", action = wezterm.action.ActivateTabRelative(-1) },
+		{ key = "]", mods = "CTRL|ALT", action = wezterm.action.ActivateTabRelative(1) },
+	},
 	native_macos_fullscreen_mode = true,
 	tab_bar_at_bottom = true,
 	use_fancy_tab_bar = false,
