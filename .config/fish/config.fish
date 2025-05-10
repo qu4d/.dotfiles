7bdf65821afb59cc82d6c521b5ba4c8013a485d1
Kirill Velichkin <k.velichkin@gmail.com>
2025-05-11 00:00:20 +0300

    Set pyenv and orbstack

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,19 @@
 ### global settings ###
 set -gx EDITOR nvim
 
-
 ### fish settings ###
 set fish_greeting
 set -g fish_key_bindings fish_vi_key_bindings
 
-
 ### other settings ###
+# pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv init - fish | source
+
+# orbstack
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
 # atac
 set -gx ATAC_KEY_BINDINGS $HOME/.config/atac/vim_key_bindings.toml
 
@@ -15,7 +21,6 @@ set -gx ATAC_KEY_BINDINGS $HOME/.config/atac/vim_key_bindings.toml
 set fzf_fd_opts --hidden --no-ignore --max-depth 5
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 fzf_configure_bindings --directory=\cF --processes=\cP --git_log= --git_status=
-
 
 ### aliases ###
 # nvim
@@ -34,6 +39,3 @@ alias lg='lazygit'
 
 # lazydocker
 alias ld='lazydocker'
-
-# orbstack
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
