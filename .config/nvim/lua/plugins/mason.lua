72018094e5faa22ea50df7b2d9094fedd7910ddb
Kirill Velichkin <k.velichkin@gmail.com>
2025-05-13 21:00:17 +0300

    Fix mason issue

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,21 +1,17 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    version = "^1.0.0",
-    opts = {
-      ensure_installed = {
-        "debugpy",
-        "delve",
-        "gofumpt",
-        "goimports",
-        "gopls",
-        "lua-language-server",
-        "pyright",
-        "ruff",
-        "shfmt",
-        "stylua",
-      },
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "debugpy",
+      "delve",
+      "gofumpt",
+      "goimports",
+      "gopls",
+      "lua-language-server",
+      "pyright",
+      "ruff",
+      "shfmt",
+      "stylua",
     },
   },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
