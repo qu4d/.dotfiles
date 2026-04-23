d2fc3635d40846741cb6dd717e6e66a8a8f7b92b
Kirill Velichkin <k.velichkin@gmail.com>
2026-04-24 01:25:11 +0300

    Rework fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,30 +1,29 @@
-### global settings ###
-set -gx EDITOR nvim
+### global ###
+set -gx EDITOR 'emacs -nw'
 
-### fish settings ###
+### fish ###
 set fish_greeting
 set -g fish_key_bindings fish_vi_key_bindings
 
-### app settings ###
-# zellij
+### zellij ###
 if status is-interactive
     set -x ZELLIJ_AUTO_ATTACH true
     set -x ZELLIJ_AUTO_EXIT true
     eval (zellij setup --generate-auto-start fish | string collect)
 end
 
-# emacs
+### emacs ###
 set -gx PATH $HOME/.emacs.d/bin $PATH
 
-# golang
+### golang ###
 set -gx GOPATH $HOME/.local/go
 set -gx PATH $GOPATH/bin $PATH
 
-# fzf
+### fzf ###
 set fzf_fd_opts --hidden --no-ignore --max-depth 5
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 fzf_configure_bindings --directory=\cF --processes=\cP --git_log= --git_status=
 
 ### aliases ###
-alias update='mas upgrade && fisher update && brew update && brew upgrade --greedy && brew autoremove && brew cleanup && brew doctor'
-alias em='emacs -nw'
+alias update='mas upgrade && fisher update && brew update && brew upgrade --greedy && brew autoremove && brew cleanup && brew doctor && doom upgrade && doom sync'
+alias ee="$EDITOR"
