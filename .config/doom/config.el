36b494e00f935369fe9ea48c12688a4c0b39d890
Kirill Velichkin <k.velichkin@gmail.com>
2026-04-29 17:54:37 +0300

    Update dashboard

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,6 +6,29 @@
 (custom-set-faces!
   '(mode-line :background "#282828")
   '(mode-line-inactive :background "#282828"))
+(defun my-dashboard-ascii-banner ()
+  (let* ((banner '(
+                   "▓█████▄  ▒█████   ▒█████   ███▄ ▄███▓"
+                   "▒██▀ ██▌▒██▒  ██▒▒██▒  ██▒▓██▒▀█▀ ██▒"
+                   "░██   █▌▒██░  ██▒▒██░  ██▒▓██    ▓██░"
+                   "░▓█▄   ▌▒██   ██░▒██   ██░▒██    ▒██ "
+                   "░▒████▓ ░ ████▓▒░░ ████▓▒░▒██▒   ░██▒"
+                   " ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░   ░  ░"
+                   " ░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░ ░  ░      ░"
+                   " ░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒  ░      ░   "
+                   "   ░        ░ ░      ░ ░         ░   "
+                   " ░                                   "
+                   ))
+         (longest (apply #'max (mapcar #'length banner))))
+    (dolist (line banner)
+      (insert
+       (propertize
+        (+doom-dashboard--center
+         +doom-dashboard--width
+         (concat line (make-string (max 0 (- longest (length line))) 32)))
+        'face '(:foreground "#98971a" :weight bold))
+       "\n"))))
+(setq +doom-dashboard-ascii-banner-fn #'my-dashboard-ascii-banner)
 
 ;; FUNCTIONALITY
 (setq display-line-numbers-type 'relative)
